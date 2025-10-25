6cb8e151a7c9629ead9461ac709a76eafbca6841
ivanperozo <ivanperozogiacomini@gmail.com>
2025-10-25 10:59:55 -0400

    Add files via upload

---
+++ PELICULAS.sql
@@ -0,0 +1,18 @@
+INSERT INTO pelicula (pelicula_id, nombre_peli, genero, estudio, director, personaje_princ) VALUES
+(1, 'Ready player one', 'ficcion', 'steven spilberg', 'parzibal'),
+(2, 'Juegos del hambre', 'distopia', 'suzane collins', 'Katniss'),
+(3, 'Los Vengadores', 'ficcion', 'Hermanos Russo', 'Los vengadores'),
+(4, 'Game of thrones', 'distopia', 'David Nutter', 'Daenerys'),
+(5, 'Harry Potter', 'distopia', 'Chris Columbus', 'Harry');
+
+
+
+CREATE TABLE peliculas (
+    nombre_peli VARCHAR(100),
+    genero VARCHAR (100),
+    estudio VARCHAR (100),
+    director VARCHAR (100),
+    personaje_princ VARCHAR (100);
+)
+
+
